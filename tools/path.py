cb34d4cd4f3cb12ea936841c3e387591d7e18629
shuang1330 <shuangli1330@gmail.com>
2017-06-14 14:43:24 +0200

    fix the path

--- tools/path.py
+++ tools/path.py
@@ -1,5 +1,5 @@
-import os.path.join as join
-PATH_DATA = join('..','data','VOCdevkit2007','VOC2007')
-PATH_FASTER_RCNN_MODEL = join('..','faster_rcnn_models',
-                        'vgg16_faster_rcnn_iter_70000')
-OUTPUT_DIR = join('..','output')
+import os.path as osp
+PATH_DATA = osp.join('..','data','VOCdevkit2007','VOC2007')
+PATH_FASTER_RCNN_MODEL = osp.join('..','faster_rcnn_models',
+                        'vgg16_faster_rcnn_iter_70000.ckpt')
+OUTPUT_DIR = osp.join('..','output')
